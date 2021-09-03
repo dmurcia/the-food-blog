5a2af99f05fd9bb8f4de6d7d63d712500aa3387e
David Ocampo <dmurcia88@gmail.com>
2021-09-02 22:35:50 -0600

    Vue and API Configuration

---
+++ api/src/server/config.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+
+module.exports = app => {
+
+  /**
+   * SETTINGS
+   */
+  app.set('port', process.env.PORT || 3000);
+
+  /**
+   * MIDDLERWARES
+   */
+  // Logger
+  app.use(morgan('dev'));
+  app.use(express.urlencoded({extended: false}));
+  app.use(express.json());
+
+  /**
+   * ROUTES
+   */
+  app.use('/api/posts', require('../routes/posts'));
+
+  return app;
+}
